01b51a1fa2baffbc58dc22f55299bc9e070c38d3
jingyuer21 <54914943+jingyuer21@users.noreply.github.com>
2020-12-23 17:18:30 -0600

    .

---
+++ app1/source.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+user = "ardit700_student",
+password = "ardit700_student",
+host = "108.167.140.122",
+database = "ardit700_pm1database"
+)
+
+cursor = con.cursor()
+
+word=input("Enter the word: ")
+
+query = cursor.execute("SELECT Definition \
+                        FROM Dictionary \
+                        WHERE Expression = '%s'" 
+                        % word)
+
+results = cursor.fetchall()
+
+if results:
+    for x in results:
+        print(x[0])
+else:
+    print("No word found!")
